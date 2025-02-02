84b3500ce1c8e1fc75e14d40433345dbfefea01b
Gregory Enriquez <enriquezgregory20@gmail.com>
2025-02-02 10:19:33 -0600

    Login update

--- src/components/ui/Login.tsx
+++ src/components/ui/Login.tsx
@@ -26,7 +26,8 @@ const Login: React.FC = () => {
       <div className="bg-white p-6 sm:p-8 rounded-2xl shadow-2xl shadow-blue-400 max-w-md w-full text-center">
         {/* Título con emoji */}
         <h1 className="text-3xl sm:text-4xl font-bold text-blue-700 mb-4">
-          📖 Biblia Reina Valera
+                 📖 
+          Biblia Reina Valera
         </h1>
         <p className="text-black text-sm sm:text-base mb-6 sm:mb-8">
           Inicia sesión para acceder a la Biblia Reina Valera.
@@ -35,7 +36,7 @@ const Login: React.FC = () => {
         {/* Botón de Google */}
         <button
           onClick={handleGoogleSignIn}
-          className="w-full flex items-center justify-center gap-2 bg-blue-300 border border-gray-400 text-gray-700 font-semibold py-2 sm:py-3 px-4 rounded-lg shadow-sm hover:shadow-md transition-all mb-4"
+          className="w-full flex items-center justify-center gap-2 bg-blue-200 border border-gray-400 text-gray-700 font-semibold py-2 sm:py-3 px-4 rounded-lg shadow-sm hover:shadow-md transition-all mb-4"
         >
           <FaGoogle className="w-5 h-5 sm:w-6 sm:h-6" />
           <span className="text-sm sm:text-base">Iniciar con Google</span>
@@ -53,5 +54,7 @@ const Login: React.FC = () => {
     </div>
   );
 };
+
+
 
 export default Login;
